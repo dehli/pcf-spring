5404336fa4839a0d4d4b684f90c5b00c5e6c18ea
Christian Paul Dehli <dehli@gatech.edu>
2017-09-07 09:55:35 -0400

    Updated build script to work with pdf cloud

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pcf_cloud=true
+
 # kotlin
 cd kotlin
 gradle wrapper --gradle-version 3.5.1
@@ -9,16 +11,29 @@ gradle wrapper --gradle-version 3.5.1
 cd ../accounts
 gradle wrapper --gradle-version 3.5.1
 ./gradlew assemble
-cf create-service p-mysql 1gb accounts-mysql
-cf create-service p-redis shared-vm accounts-redis
+if [ "$pcf_cloud" = true ] ; then
+  cf create-service cleardb spark accounts-mysql
+else
+  cf create-service p-mysql 1gb accounts-mysql
+fi
 
 # transactions
 cd ../transactions
 gradle wrapper --gradle-version 3.5.1
 ./gradlew assemble
-cf create-service p-mysql 1gb transactions-mysql
-cf create-service p-redis shared-vm transactions-redis
+if [ "$pcf_cloud" = true ] ; then
+  cf create-service cleardb spark transactions-mysql
+else
+  cf create-service p-mysql 1gb transactions-mysql
+fi
 
 # shared services
-cf create-service p-rabbitmq standard events-queue
+if [ "$pcf_cloud" = true ] ; then
+  cf create-service cloudamqp lemur events-queue
+  cf create-service rediscloud 30mb shared-redis
+else
+  cf create-service p-rabbitmq standard events-queue
+  cf create-service p-redis shared-vm shared-redis
+fi
+
 cf create-service -c '{ "git": { "uri": "https://github.com/dehli/pcf-spring.git", "label": "master"  } }' p-config-server standard config-server
